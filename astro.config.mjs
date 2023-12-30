a4c6044bae2d1f2586cd2c22852b02d636c60731
UnCompa <brandonddxd@gmail.com>
2023-12-30 10:30:16 -0500

    Preparando Todo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   base: '/Spotify-Clon/',
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
